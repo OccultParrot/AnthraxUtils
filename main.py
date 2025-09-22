c3291b4df51f7fc0f649eb05594aa0f6ccd1876c
Thomas Stemler <stemlertho@gmail.com>
2025-09-21 19:35:16 -0500

    Added age command

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import datetime
 
+import discord
 from dotenv import load_dotenv
 from rich.console import Console
-from discord import Client, Intents, app_commands, Object
+from discord import Client, Intents, app_commands, Object, Interaction, Embed
 
 load_dotenv()
 console = Console()
@@ -25,6 +27,29 @@ client = AnthraxUtilsClient()
 @client.event
 async def on_ready():
     console.print(f"Logged in as [green]{client.user.name}[/green]", justify="center")
+
+@client.tree.command(name="calculate-age", description="Calculate the age of your dino, using their birthdate.")
+@app_commands.describe(birthdate="In YYYY-MM-DD format")
+async def calculate_age(interaction: Interaction, birthdate: str):
+    try:
+        birth_date = datetime.datetime.fromisoformat(birthdate).date()
+        difference = abs((birth_date - datetime.date.today()).days)
+        age = difference // 7
+        
+        embed = Embed(title="Age of Dino", description=f"""\
+Age in Weeks: `{age} week(s)`
+Age in in-game years: `{age //4} year(s)`
+""", color=discord.Color.greyple())
+        embed.add_field(name="Today's Date", value=datetime.date.today().strftime("%d-%m-%Y"), inline=True)
+        embed.add_field(name="Birthdate", value=birth_date.strftime("%d-%m-%Y"), inline=True)
+        embed.set_footer(text="Each in-game year is 4 weeks long.")
+        
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        
+    except ValueError as e:
+        await interaction.response.send_message("Invalid date format. Please use DD-MM-YYYY.", ephemeral=True)
+        return
+    
 
 # == Running the bot ==
 client.run(os.getenv("TOKEN"))
